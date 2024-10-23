d189d8499e4356e5f8c94402d9a6a2489ad28553
liza <lizabelobrova0@gmail.com>
2024-10-23 21:47:22 +0500

    Модуляция

---
+++ js/util.js
@@ -0,0 +1,11 @@
+const getRandomInteger = (min, max) => {
+  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
+  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+
+  return Math.floor(result);
+};
+
+const getRandomElement = (items) => items[getRandomInteger(0, items.length - 1)];
+
+export { getRandomElement, getRandomInteger };
