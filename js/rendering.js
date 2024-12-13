281dbafd87ebd190d9294f5f3799e40b3be36e0d
liza <lizabelobrova0@gmail.com>
2024-12-13 13:53:56 +0500

    Надо подкачаться

--- js/rendering.js
+++ js/rendering.js
@@ -1,8 +1,11 @@
-import {openBigPicture} from './big-picture.js';
-import {getPhoto} from './data.js';
+import { openBigPicture } from './big-picture.js';
+import { getPhotos } from './data.js';
 
 const thumbnailTemplate = document.querySelector('#picture').content.querySelector('.picture');
 const container = document.querySelector('.pictures');
+const errorTemplate = document.querySelector('#error').content.querySelector('.error');
+const errorButton = errorTemplate.querySelector('.error__button');
+const body = document.querySelector('body');
 
 const createThumbnail = ({ url, description, likes, comments, id }) => {
   const thumbnail = thumbnailTemplate.cloneNode(true);
@@ -21,10 +24,24 @@ const createThumbnail = ({ url, description, likes, comments, id }) => {
   return thumbnail;
 };
 
-const postsDataset = getPhoto();
+const showError = (message) => {
+  const errorElement = errorTemplate.cloneNode(true);
+  errorElement.querySelector('.error__title').textContent = message;
+  errorButton.addEventListener('click', () => {
+    errorElement.remove();
+  });
+  body.append(errorElement);
+};
 
-const renderThumbnails = () => {
-  postsDataset.forEach((data) => container.append(createThumbnail(data)));
+const renderThumbnails = async () => {
+  try {
+    const photos = await getPhotos();
+    photos.forEach((photo) => {
+      container.append(createThumbnail(photo));
+    });
+  } catch (error) {
+    showError(error.message);
+  }
 };
 
 export { renderThumbnails };
